d07c0f2374ac5f362b716c1a75de941afbf03b71
Grzegorz <grzegorzkowalczyk1989@gmail.com>
2021-10-07 15:07:24 +0200

    6.2 Added kodilla-testing Calculator

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package com.kodilla.testing.calculator;
+
+
+
+public class Calculator {
+    public static void main(String[] args) {
+    }
+
+        public int getSum(int a, int b) {
+            return a + b;
+        }
+
+        public int getSub(int a, int b) {
+            return a - b;
+        }
+    }
+
+
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.user;
+
+public class SimpleUser {
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public SimpleUser(String username) {
+        this.username = username;
+
+    }
+}
