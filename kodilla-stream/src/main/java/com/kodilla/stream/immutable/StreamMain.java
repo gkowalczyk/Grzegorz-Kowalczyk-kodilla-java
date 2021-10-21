845f54fdab01dfa9daffb6bae2f7c3ea31bd1b93
Grzegorz <grzegorzkowalczyk1989@gmail.com>
2021-10-21 07:04:30 +0200

    7.2 Added kodilla-stream ForumUser

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/StreamMain.java
@@ -0,0 +1,4 @@
+package com.kodilla.stream.immutable;
+
+public class StreamMain {
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/iterate/StreamMain.java
@@ -0,0 +1,11 @@
+package com.kodilla.stream.iterate;
+
+public class StreamMain {
+    public static void main(String[] args) {
+
+        System.out.println("generate even numbers");
+        NumbersGenerator.generateEven(20);
+    }
+
+
+}
