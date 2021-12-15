1be86bdee93e519d9f2f2a995dd07d435eb875a7
Grzesiek <grzegorzkowalczyk1989@gmail.com>
2021-12-15 07:06:07 +0100

    14.2 Added kodilla-pattern- spring - Calculator

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -0,0 +1,35 @@
+package com.kodilla.spring.calculator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+
+    @Autowired
+    private Display display;
+
+    public double add(double a, double b) {
+        double val = a + b;
+        display.displayValue(val);
+        return val;
+    }
+
+    public double sub(double a, double b) {
+        double val = a - b;
+        display.displayValue(val);
+        return val;
+    }
+
+    public double mul(double a, double b) {
+        double val = a * b;
+        display.displayValue(val);
+        return val;
+    }
+
+    public double div(double a, double b) {
+        double val = a / b;
+        display.displayValue(val);
+        return val;
+    }
+}
