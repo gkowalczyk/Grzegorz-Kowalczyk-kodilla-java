7df63e4010ffbb97bf613af4adc8cb4d02eeefcf
Grzesiek <grzegorzkowalczyk1989@gmail.com>
2021-12-19 18:53:07 +0100

    14.4 Added kodilla-pattern- spring web

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Food2Door/ExtraFoodExample.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Food2Door/ExtraFoodExample.java
@@ -1,16 +1,16 @@
 package com.kodilla.good.patterns.challenges.Food2Door;
 
-public class ExtraFoodExample extends Product{
+//public class ExtraFoodExample extends Product{
 
-    private String productExplain;
+   // private String productExplain;
 
-    public String getProductExplain() {
-        return productExplain;
-    }
+   // public String getProductExplain() {
+    //    return productExplain;
+   // }
 
-    public ExtraFoodExample(String name, String productExplain) {
-        super(name);
-        this.productExplain = productExplain;
+    //public ExtraFoodExample(String name, String productExplain) {
+        //super(name);
+        //this.productExplain = productExplain;
 
-    }
-}
+  //  }
+//}
---
+++ kodilla-spring-web/src/test/java/com/kodilla/kodillaspringweb/KodillaSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillaspringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaSpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
